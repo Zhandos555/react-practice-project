91144e6e47f0bc2b826fd9593bf050883a723fa6
Zhandos Sembayev <sembayev.zhandos@gmail.com>
2022-05-31 18:21:48 +0600

    fix: fixed components MealsItem, MealItemForm

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,10 @@
-import React from "react";
-import reactDom from "react-dom";
-import ReactDom from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -15,20 +15,17 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
-    <React.Fragment>
-      {reactDom.createPortal(
-        <Backdrop onClose={props.onClose} />,
-        portalElement
-      )}
-      {reactDom.createPortal(
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
-    </React.Fragment>
+    </Fragment>
   );
 };
 
